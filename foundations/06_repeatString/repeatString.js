04103fc116464e0b1d20965a2af5476ff8f45c03
OfriAmir <274250321+OfriAmir@users.noreply.github.com>
2026-05-05 15:34:42 +0300

    Complete exercise 6

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,6 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    if (num<0) return 'ERROR'
+    let repeatedString = ''
+    while (num) {
+        repeatedString += string
+        num--
+    }
+    return repeatedString
 };
+
+console.log(repeatString('hey', 3))
 
 // Do not edit below this line
 module.exports = repeatString;
