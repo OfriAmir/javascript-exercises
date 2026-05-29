3a4fba80787219785f987ee7c618ac5cbb2749ba
OfriAmir <274250321+OfriAmir@users.noreply.github.com>
2026-05-29 13:08:45 +0300

    Complete foundations js exercises 10-11

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(tempF) {
+  const tempC = (tempF - 32) / 1.8
+  return +tempC.toFixed(1)
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempC) {
+  const tempF = 1.8 * tempC + 32
+  return +tempF.toFixed(1)
 };
 
 // Do not edit below this line
