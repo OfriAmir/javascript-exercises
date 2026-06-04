03677a26694a219d45c0c678f73b575307fa794a
OfriAmir <274250321+OfriAmir@users.noreply.github.com>
2026-06-04 21:19:58 +0300

    Finish ex.12

--- foundations/12_calculator/calculator.js
+++ foundations/12_calculator/calculator.js
@@ -1,25 +1,31 @@
-const add = function() {
-	
+const add = function(a,b) {
+	return a + b
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return a - b
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  if (!arr) return 0
+	return arr.reduce((total, current) => total + current, 0)
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  if (!arr) return 0
+  return arr.reduce((total, current) => total * current)
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+  let product = 1
+	for (let i = 0; i < b; i++) product *=a
+  return product
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+	let product = 1
+  for (let i = 0; i < a; i++) product = product*(a-i)
+  return product
 };
 
 // Do not edit below this line
