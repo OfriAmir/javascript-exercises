b62bdd37049dd8384d597dfc46260339b9f2fb37
OfriAmir <274250321+OfriAmir@users.noreply.github.com>
2026-06-05 14:48:29 +0300

    Finish ex. 15-16

--- foundations/15_getTheTitles/getTheTitles.js
+++ foundations/15_getTheTitles/getTheTitles.js
@@ -1,5 +1,12 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(arr) {
+    //one way:
+    /*let arrTitles = []
+    for (let obj of arr){
+        arrTitles.push(obj.title)
+    }
+    return arrTitles*/
+    //easier:
+    return arr.map(obj => obj.title)
 };
 
 // Do not edit below this line
--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,6 +1,15 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(arr) {
+    let Oldest = arr.reduce((currentOldest, current) => {
+        let currentAge = 0
+        if (!current.yearOfDeath) currentAge = (new Date()).getFullYear() - current.yearOfBirth
+        else currentAge = current.yearOfDeath - current.yearOfBirth
+        let currentOldestAge = 0
+        if (!currentOldest.yearOfDeath) currentOldestAge = (new Date()).getFullYear() - currentOldest.yearOfBirth
+        else currentOldestAge = currentOldest.yearOfDeath - currentOldest.yearOfBirth
+        if (currentAge > currentOldestAge) return current
+        else return currentOldest
+    })
+    return Oldest
 };
-
 // Do not edit below this line
 module.exports = findTheOldest;
