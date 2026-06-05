90976c3836ec22a5360093ee756bb93dd0e1be1c
OfriAmir <274250321+OfriAmir@users.noreply.github.com>
2026-06-05 13:04:23 +0300

    Finish ex. 13-14

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,6 +1,31 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    let irrelevant =[".",",","."," ",":",";","?","!",")","(","'"]
+    let newStr = ""
+    for (let i=0; i < str.length; i++){
+        if (!irrelevant.includes(str[i])){
+            newStr += str[i]
+        }
+    }
+    //bad way:
+    /*if (newStr.length % 2 == 0){
+        console.log((newStr.slice(0, newStr.length/2)).toLowerCase())
+        console.log(((newStr.slice(newStr.length/2)).toLowerCase()))
+        if ((newStr.slice(0, newStr.length/2)).toLowerCase()
+        == [...(newStr.slice(newStr.length/2)).toLowerCase()].reverse().join("")){
+            return true
+        }
+    }
+    console.log((newStr.slice((newStr.length + 1) / 2)).toLowerCase())        
+    if ((newStr.slice(0, (newStr.length - 1) / 2)).toLowerCase()
+        == [...(newStr.slice((newStr.length + 1) / 2)).toLowerCase()].reverse().join("")){
+        
+        return true     
+    }
+    */
+    //Much better way. still worse than the proposed solution...
+    if (newStr.toLowerCase() == newStr.split('').reverse().join("").toLowerCase()) return true
+    return false
 };
-
+console.log(palindromes('ZZZZ car, a man, a maracaz.'))
 // Do not edit below this line
 module.exports = palindromes;
--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,6 +1,16 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    if (num < 0) return 'OOPS'
+    let sum1 = 1
+    let sum2 = 0
+    let sum3 = 0
+    for (let i=0; i<num; i++){    
+        sum3 = sum2 + sum1
+        sum1 = sum2
+        sum2 = sum3
+    }
+    return sum3
 };
+//console.log()
 
 // Do not edit below this line
 module.exports = fibonacci;
